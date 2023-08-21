c37a65e31cda871827f9bbdbc41faf56726890e2
PLK <unrecht.official@gmail.com>
2023-08-21 22:58:51 +0200

    🎉 Initial commit.

---
+++ src/main/java/de/plk/plugin/ProxyPlugin.java
@@ -0,0 +1,23 @@
+package de.plk.plugin;
+
+import de.plk.core.api.AbstractSpicyProxy;
+
+/**
+ * @author SoftwareBuilds
+ * @since 21.08.2023 22:57
+ * Copyright © 2023 | SoftwareBuilds | All rights reserved.
+ */
+public final class ProxyPlugin extends AbstractSpicyProxy {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+
+    }
+}
